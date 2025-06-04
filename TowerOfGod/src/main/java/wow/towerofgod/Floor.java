f7c43706ea039b10592c47e84d41295b72f90d4d
najeebiee <nmapantas_240000002090@uic.edu.ph>
2025-06-04 15:23:51 +0800

    Add files via upload

---
+++ TowerOfGod/src/main/java/wow/towerofgod/Floor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wow.towerofgod;
+
+/**
+ *
+ * @author Students Account
+ */
+public abstract class Floor {
+    
+    protected String floorName;
+    public abstract void enter();
+    
+}
