b4ac18e2072b96ed8ac2de3b52b1898d58458e98
najeebiee <nmapantas_240000002090@uic.edu.ph>
2025-06-09 13:32:12 +0800

    Update Player.java

--- TowerOfGod/src/main/java/wow/towerofgod/Player.java
+++ TowerOfGod/src/main/java/wow/towerofgod/Player.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package wow.towerofgod;
 
-/**
- *
- * @author Students Account
- */
 public class Player {
+    
+    private String name;
+    int shinsooLevel;
+    int currentFloor;
+    
+    public Player(String name, int shinsooLevel, int currentFloor) {
+        this.name = name;
+        this.shinsooLevel = shinsooLevel;
+        this.currentFloor = currentFloor;
+    }
+    
+    public void drainShinsoo(int amount) {
+        shinsooLevel -= amount;
+        if (shinsooLevel < 0) shinsooLevel = 0;
+    }
+    
+    public static void restoreShinsoo(int amount) {
+        
+    }
+    
+    public boolean isAlive() {
+        
+        return shinsooLevel > 0;
+        
+    }
+    
+    public int getShinsooLevel() {
+        return shinsooLevel;
+    }
+    
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void advanceFloor() {
+        currentFloor++;
+    }
     
 }
